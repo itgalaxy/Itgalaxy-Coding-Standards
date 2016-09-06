18895227a8dc1bd1f31de4d8aab0057fdece4200
Alexander Krasnoyarov <alexander.krasnoyarov@itgalaxy.company>
2016-09-06 19:02:52 +0300

    Chore: install `xdebug` and testing on php `5.6` and `7.0`.

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -9,10 +9,7 @@ set -xe
 apt-get update -yqq
 apt-get install git -yqq
 
-# Install phpunit, the tool that we will use for testing
-curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
-chmod +x /usr/local/bin/phpunit
-
-# Install mysql driver
+# Install xdebug
 # Here you can install any other extension that you need
-docker-php-ext-install xdebug
+pecl install xdebug
+docker-php-ext-enable xdebug
