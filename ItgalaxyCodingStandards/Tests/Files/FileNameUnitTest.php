af7c9cc703bf3ea96a923824ed48a28851f2fccd
Alexander Krasnoyarov <alexander.krasnoyarov@itgalaxy.company>
2016-09-06 19:39:33 +0300

    Chore: improved coverage.

---
+++ ItgalaxyCodingStandards/Tests/Files/FileNameUnitTest.php
@@ -0,0 +1,39 @@
+<?php
+
+class ItgalaxyCodingStandards_Tests_Files_FileNameUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getErrorList($testFile = '')
+    {
+        switch ($testFile) {
+            case 'FileNameUnitTest.1.inc':
+                return [
+
+                ];
+            case 'FileNameUnitTest.2_.inc':
+                return [1];
+            default:
+                return [];
+        }
+    }
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getWarningList()
+    {
+        return [];
+    }
+}
