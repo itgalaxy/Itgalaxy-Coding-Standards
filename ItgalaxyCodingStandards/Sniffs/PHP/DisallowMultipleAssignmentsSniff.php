69cfd584e0d802f64065379fdd25dc6ae29a1c09
Alexander Krasnoyarov <alexander.krasnoyarov@itgalaxy.company>
2016-09-16 12:46:41 +0300

    Added: `DuplicateSemicolon` sniff.

--- ItgalaxyCodingStandards/Sniffs/PHP/DisallowMultipleAssignmentsSniff.php
+++ ItgalaxyCodingStandards/Sniffs/PHP/DisallowMultipleAssignmentsSniff.php
@@ -1,32 +1,4 @@
 <?php
-/**
- * Ongr_Sniffs_PHP_DisallowMultipleAssignmentsSniff.
- *
- * PHP version 5
- *
- * @category  PHP
- * @package   PHP_CodeSniffer
- * @author    Greg Sherwood <gsherwood@squiz.net>
- * @author    Marc McIntyre <mmcintyre@squiz.net>
- * @copyright 2006-2014 Squiz Pty Ltd (ABN 77 084 670 600)
- * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
- * @link      http://pear.php.net/package/PHP_CodeSniffer
- */
-/**
- * Ongr_Sniffs_PHP_DisallowMultipleAssignmentsSniff.
- *
- * Ensures that there is only one value assignment on a line, and that it is
- * the first thing on the line.
- *
- * @category  PHP
- * @package   PHP_CodeSniffer
- * @author    Greg Sherwood <gsherwood@squiz.net>
- * @author    Marc McIntyre <mmcintyre@squiz.net>
- * @copyright 2006-2014 Squiz Pty Ltd (ABN 77 084 670 600)
- * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
- * @version   Release: @package_version@
- * @link      http://pear.php.net/package/PHP_CodeSniffer
- */
 
 namespace ItgalaxyCodingStandards\Sniffs\PHP;
 
