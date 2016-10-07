3a831a91339e2a77b8c207f8d16a81210059462d
Alexander Krasnoyarov <alexander.krasnoyarov@itgalaxy.company>
2016-10-07 19:36:40 +0300

    Reference: issue #5.

--- ItgalaxyCodingStandards/Sniffs/Formatting/SemicolonBeforeClosePHPTagSniff.php
+++ ItgalaxyCodingStandards/Sniffs/Formatting/SemicolonBeforeClosePHPTagSniff.php
@@ -1,28 +1,7 @@
 <?php
-/**
- * Drupal_Sniffs_Formatting_SpaceUnaryOperatorSniff.
- *
- * PHP version 5
- *
- * @category PHP
- * @package  PHP_CodeSniffer
- * @link     http://pear.php.net/package/PHP_CodeSniffer
- */
-
-/**
- * Drupal_Sniffs_Formatting_SpaceUnaryOperatorSniff.
- *
- * Ensures there are no spaces on increment / decrement statements or on +/- sign
- * operators or "!" boolean negators.
- *
- * @category PHP
- * @package  PHP_CodeSniffer
- * @link     http://pear.php.net/package/PHP_CodeSniffer
- */
-
 namespace ItgalaxyCodingStandards\Sniffs\Formatting;
 
-class SemicolonSniff implements \PHP_CodeSniffer_Sniff
+class SemicolonBeforeClosePHPTagSniff implements \PHP_CodeSniffer_Sniff
 {
     /**
      * A list of tokenizers this sniff supports.
@@ -45,8 +24,8 @@ class SemicolonSniff implements \PHP_CodeSniffer_Sniff
      * Processes this test, when one of its tokens is encountered.
      *
      * @param \PHP_CodeSniffer_File $phpcsFile The file being scanned.
-     * @param int                  $stackPtr  The position of the current token in
-     *                                        the stack passed in $tokens.
+     * @param int                   $stackPtr  The position of the current token in
+     *                                         the stack passed in $tokens.
      *
      * @return void
      */
--- ItgalaxyCodingStandards/Tests/Formatting/SemicolonBeforeClosePHPTagUnitTest.php
+++ ItgalaxyCodingStandards/Tests/Formatting/SemicolonBeforeClosePHPTagUnitTest.php
@@ -1,6 +1,6 @@
 <?php
 
-class ItgalaxyCodingStandards_Tests_Formatting_SemicolonUnitTest extends AbstractSniffUnitTest
+class ItgalaxyCodingStandards_Tests_Formatting_SemicolonBeforeClosePHPTagUnitTest extends AbstractSniffUnitTest
 {
     /**
      * Returns the lines where errors should occur.
