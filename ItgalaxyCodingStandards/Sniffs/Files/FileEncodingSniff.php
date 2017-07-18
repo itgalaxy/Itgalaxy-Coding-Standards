d5303eac5adee344d7553851f7796c0164b00135
evilebottnawi <alexander.krasnoyarov@itgalaxy.company>
2017-07-18 22:57:32 +0300

    Feat: improve sniffs.

---
+++ ItgalaxyCodingStandards/Sniffs/Files/FileEncodingSniff.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * FileEncodingSniff.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+namespace ItgalaxyCodingStandards\Sniffs\Files;
+
+/**
+ * FileEncodingSniff.
+ *
+ * Validates the encoding of a file against a white list of allowed encodings.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class FileEncodingSniff implements \PHP_CodeSniffer_Sniff
+{
+    /**
+     * List of encodings that files may be encoded with.
+     *
+     * Any other detected encodings will throw a warning.
+     *
+     * @var array
+     */
+    public $allowedEncodings = ['UTF-8'];
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return [
+            T_INLINE_HTML,
+            T_OPEN_TAG,
+        ];
+    }
+
+    /**
+     * Processes this sniff, when one of its tokens is encountered.
+     *
+     * @param \PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token in
+     *                                        the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(\PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        // Not all PHP installs have the multi byte extension - nothing we can do.
+        if (function_exists('mb_check_encoding') === false) {
+            return;
+        }
+
+        $fileContent = $phpcsFile->getTokensAsString(0, $phpcsFile->numTokens);
+        $validEncodingFound = false;
+
+        foreach ($this->allowedEncodings as $encoding) {
+            if (mb_check_encoding($fileContent, $encoding) === true) {
+                $validEncodingFound = true;
+            }
+        }
+
+        if ($validEncodingFound === false) {
+            $warning = 'File encoding is invalid, expected %s';
+            $data = [implode(' or ', $this->allowedEncodings)];
+            $phpcsFile->addWarning($warning, $stackPtr, 'InvalidEncoding', $data);
+        }
+    }
+}
---
+++ ItgalaxyCodingStandards/Sniffs/WhiteSpace/CloseBracketSpacingSniff.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * CloseBracketSpacingSniff.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @link     http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+namespace ItgalaxyCodingStandards\Sniffs\WhiteSpace;
+
+/**
+ * Checks that there is no white space before a closing bracket, for ")" and "}".
+ * Square Brackets are handled by Squiz_Sniffs_Arrays_ArrayBracketSpacingSniff.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @link     http://pear.php.net/package/PHP_CodeSniffer
+ */
+class CloseBracketSpacingSniff implements \PHP_CodeSniffer_Sniff
+{
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return [
+            T_CLOSE_CURLY_BRACKET,
+            T_CLOSE_PARENTHESIS,
+            T_CLOSE_SHORT_ARRAY,
+        ];
+    }
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param \PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token
+     *                                        in the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(\PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        if (isset($tokens[$stackPtr - 1]) === true
+            && $tokens[$stackPtr - 1]['code'] === T_WHITESPACE
+        ) {
+            $before = $phpcsFile->findPrevious(\PHP_CodeSniffer_Tokens::$emptyTokens, $stackPtr - 1, null, true);
+
+            if ($before !== false && $tokens[$stackPtr]['line'] === $tokens[$before]['line']) {
+                $error = 'There should be no white space before a closing "%s"';
+                $fix = $phpcsFile->addFixableError(
+                    $error,
+                    $stackPtr - 1,
+                    'ClosingWhitespace',
+                    [$tokens[$stackPtr]['content']]
+                );
+
+                if ($fix === true) {
+                    $phpcsFile->fixer->replaceToken($stackPtr - 1, '');
+                }
+            }
+        }
+    }
+}
